41565d6e42b97c4a3d133a828b220ebd7c841501
f4rh4d <farhadf4rh4di@gmail.com>
2021-03-14 12:46:01 +0330

    added back menu

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -356,6 +356,25 @@ const Home = () => {
   );
 };
 
+const menu = (
+  <svg
+    xmlns="http://www.w4.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="30"
+    height="30"
+    fill="none"
+    stroke="#FFF"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-menu menu-animation menu-button"
+  >
+    <line x1="3" y1="12" x2="21" y2="12"></line>
+    <line x1="3" y1="6" x2="21" y2="6"></line>
+    <line x1="3" y1="18" x2="21" y2="18"></line>
+  </svg>
+);
+
 const success = (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
     <circle cx="25" cy="25" r="25" fill="#25e1a1" />
