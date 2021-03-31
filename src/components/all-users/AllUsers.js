a8bd8a34f016ea6a0f801f822cbbdd1cdbca5a0d
Amir Farhadi <cnooed@gmail.com>
2021-03-31 07:38:47 +0430

    added edit and delete button

--- src/components/all-users/AllUsers.js
+++ src/components/all-users/AllUsers.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Axios from 'axios';
 
 const AllUsers = () => {
   const [users, setUsers] = useState([
@@ -7,6 +8,27 @@ const AllUsers = () => {
     { username: 'user2', userId: 'user2', pass: '123', key: 2 },
     { username: 'user3', userId: 'user3', pass: '123', key: 3 },
   ]);
+  const changeHandler = async () => {
+    try {
+      const result = await Axios.post();
+    } catch (error) {
+    } finally {
+    }
+  };
+  const deleteUserHandler = async () => {
+    try {
+      const result = await Axios.post();
+    } catch (error) {
+    } finally {
+    }
+  };
+  const addUserHandler = async () => {
+    try {
+      const result = await Axios.post();
+    } catch (error) {
+    } finally {
+    }
+  };
 
   return (
     <>
@@ -20,6 +42,11 @@ const AllUsers = () => {
           </Column>
           <Column>
             <ColumnsTitle>رمز عبور</ColumnsTitle>
+          </Column>
+          <Column>
+            <Button onClick={addUserHandler} color="green">
+              <ButtonText>افزودن کاربر</ButtonText>
+            </Button>
           </Column>
         </ColumnsSection>
 
@@ -35,6 +62,14 @@ const AllUsers = () => {
 
               <Column>
                 <DataValue>{user.pass}</DataValue>
+              </Column>
+              <Column>
+                <Button onClick={deleteUserHandler} color="black">
+                  <ButtonText>حذف</ButtonText>
+                </Button>
+                <Button onClick={changeHandler} color="gray">
+                  <ButtonText>به روز رسانی</ButtonText>
+                </Button>
               </Column>
             </DataRow>
           ))}
@@ -121,6 +156,48 @@ const ColumnsSection = styled.div`
   width: 100%;
   padding: 14px 48px 10px 48px;
 `;
+const Button = styled.div`
+  pointer-events: auto;
+  transition: all 0.2s ease;
+  &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
+
+  ${(props) =>
+    props.color === 'gray'
+      ? css`
+          background: var(--dove-gray);
+        `
+      : props.color === 'green'
+      ? css`
+          background: var(--caribbean-green);
+        `
+      : css`
+          background: black;
+        `}
+
+  border-radius: 12px;
+  height: 30px;
+  width: 110px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-left: 16px;
+`;
+
+const ButtonText = styled.p`
+  color: white;
+  font-family: 'Dana-Regular', Helvetica, Arial, serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  margin: 0;
+`;
+
 const ColumnsTitle = styled.p`
   color: black;
   font-family: 'Dana-Regular', Helvetica, Arial, serif;
