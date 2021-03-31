837904170a81b37cd073dc0691f036b3beabe5e8
f4rh4d <farhadf4rh4di@gmail.com>
2021-03-31 11:56:55 +0430

    added all-user

--- src/components/all-users/AllUsers.js
+++ src/components/all-users/AllUsers.js
@@ -1,8 +1,11 @@
+import Axios from 'axios';
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
-import Axios from 'axios';
+import SideMenu from '../side-menu/SideMenu';
 
 const AllUsers = () => {
+  const [isAdmin, setIsAdmin] = useState(true);
+  const [visible, setVisible] = useState(false);
   const [users, setUsers] = useState([
     { username: 'user1', userId: 'user1', pass: '123', key: 1 },
     { username: 'user2', userId: 'user2', pass: '123', key: 2 },
@@ -32,6 +35,15 @@ const AllUsers = () => {
 
   return (
     <>
+      <SideMenuContainer visible={visible}>
+        <SideMenu />
+      </SideMenuContainer>
+      <BlurContainer onClick={() => setVisible(false)} visible={visible} />
+
+      <Header isAdmin={isAdmin}>
+        <div onClick={() => setVisible(!visible)}>{menu}</div>
+        <p>کشت و صنعت اکسون</p>
+      </Header>
       <Wrapper>
         <ColumnsSection>
           <Column>
@@ -78,6 +90,114 @@ const AllUsers = () => {
     </>
   );
 };
+
+const SideMenuContainer = styled.div`
+  top: 0px;
+  right: -333px;
+  width: 333px;
+  height: 100%;
+  position: fixed;
+  z-index: 11;
+  transition: all 0.8s ease;
+  ${(props) =>
+    props.visible
+      ? css`
+          transform: translate(-333px, 0);
+        `
+      : css`
+          transform: translate(333px, 0);
+        `}
+`;
+
+const menu = (
+  <svg
+    xmlns="http://www.w4.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="30"
+    height="30"
+    fill="none"
+    stroke="#FFF"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-menu menu-animation menu-button"
+  >
+    <line x1="3" y1="12" x2="21" y2="12"></line>
+    <line x1="3" y1="6" x2="21" y2="6"></line>
+    <line x1="3" y1="18" x2="21" y2="18"></line>
+  </svg>
+);
+
+const BlurContainer = styled.div`
+  with: 100%;
+  height: 100%;
+  background-color: rgb(0, 0, 0); /* Fallback color */
+
+  background-color: rgba(0, 0, 0, 0.6);
+  /* Overlay effect: translucent background: black w/ partial opacity */
+
+  width: 100vw; /* Full width */
+  height: 100vh; /* Full height */
+  position: fixed; /* Fix position on the top-left corner*/
+  top: 0;
+  left: 0;
+  overflow: auto; /* Enable scroll if needed */
+  backdrop-filter: blur(3px);
+  transition: all 0.8s ease;
+  z-index: 9;
+
+  ${(props) =>
+    props.visible
+      ? css`
+          visibility: visible;
+          opacity: 1;
+        `
+      : css`
+          visibility: hidden;
+          opacity: 0;
+        `}
+`;
+
+const Header = styled.div`
+  position: fixed;
+  top: 0;
+  z-index: 10;
+
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-start;
+  align-items: center;
+
+  width: 100vw;
+  height: 70px;
+  min-height: 70px;
+  max-height: 70px;
+  padding-right: 48px;
+
+  p {
+    color: white;
+
+    letter-spacing: 0px;
+    text-align: center;
+    white-space: nowrap;
+
+    font-family: 'Dana-Bold', Helvetica, Arial, serif;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+
+    margin-right: 16px;
+  }
+
+  ${(props) =>
+    props.isAdmin
+      ? css`
+          background-color: rgba(112, 112, 112, 1) !important ;
+        `
+      : css`
+          background-color: var(--caribbean-green);
+        `}
+`;
 
 const Wrapper = styled.div`
   display: flex;
